2dc99130e8cb87bcd67d982a1aa313aca4ed9066
Adrian <adi@AdiBrunnerMBP15.local>
2014-11-01 17:42:44 +0100

    commited 1st of November

---
+++ ex42.rb
@@ -0,0 +1,21 @@
+## Animals is-a object look at the extra credt
+class Animal
+end
+
+## ??
+class Dog < Animal
+  
+  def initialize(name)
+    ## ??
+    @name = name
+  end
+end
+
+## ??
+class Cat < Animal
+  
+  def initialize(name)
+    ## ??
+    @name = name
+  end
+end
