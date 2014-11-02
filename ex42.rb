5a8fe79c3a07c1119cb1e48007bf269c65eb0682
Adrian <adi@AdiBrunnerMBP15.local>
2014-11-02 12:00:28 +0100

    commited 2nd Nov 12pm

--- ex42.rb
+++ ex42.rb
@@ -19,3 +19,70 @@ class Cat < Animal
     @name = name
   end
 end
+
+## ??
+class Person
+  
+  def initialize(name)
+    ## ??
+    @name = name
+    
+    ## Person has-a pet of some kind
+    @pet = nil
+  end
+  
+  attr_accessor :pet
+end
+
+## ??
+class Employee < Person
+  
+  def initialize(name, salary)
+    ## ?? hmm what is this strange magic?
+    # get the name the same way as you do in the class Person
+    super(name)
+    ## ??
+    @salary = salary
+  end
+
+end
+
+## ??
+class Fish
+end
+
+## ??
+class Salmon < Fish
+end
+
+## ??
+class Halibut < Fish
+end
+
+
+## rover is-a Dog
+rover = Dog.new("Rover")
+
+## satan is-a Cat
+satan = Cat.new("Satan")
+
+## mary is-a Person
+mary = Person.new("Mary")
+
+## ??
+mary.pet = satan
+
+## frank is-a Employee named Frank with a salary of 120'000
+frank = Employee.new("Frank", 120000)
+
+## franks has-a pet named rover
+frank.pet = rover
+
+## flipper is-a fish
+flipper = Fish.new()
+
+## crouse is-a salmon
+crouse = Salmon.new()
+
+## harry is-a halibut
+harry = Halibut.new()
