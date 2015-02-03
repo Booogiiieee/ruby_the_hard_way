7847c89d6ee9716abf9c27b0ff326e87c50f0ffa
ADD <adi.brunner@gmx.net>
2015-02-03 16:36:57 +0100

    restored 'Hello world'

--- ex1.rb
+++ ex1.rb
@@ -1,10 +1,10 @@
-#puts "Hello World!"
+puts "Hello World!"
 #puts "Hello Again"
 #puts "I like typing this."
 #puts "This is fun."
 #puts 'Yay! Printing.'
 #puts "I'd much rather you 'not'."
 #puts 'I "said" do not touch this.'
-puts "Extra Credit 1: Print another line"
+#puts "Extra Credit 1: Print another line"
 
 # <-- comment function 
